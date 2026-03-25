d837e4707a81dcd7db83b07d108daa89e7e33dd8
Sasha <jsenjoyer1@gmail.com>
2026-03-25 10:09:00 +0300

    chore: Инициализировал проект добавил конфиги eslint, prettier, husky и настройки ci/cd

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Привет! Это vue-education-shop</h1>
+    <p>Проект инициализирован успешно.</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+  useHead({
+    title: 'Home',
+  })
+</script>
