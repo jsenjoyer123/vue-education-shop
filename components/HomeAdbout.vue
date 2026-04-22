a055906f6abe9121523fb993f214d8cf3e8a320d
Sasha <jsenjoyer1@gmail.com>
2026-04-22 12:47:44 +0300

    feat: свертсал десктопную версию блока about

--- components/HomeAdbout.vue
+++ components/HomeAdbout.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about-container">
-    <h1>Adbout</h1>
+    <h1>About</h1>
     <h2>Who we are and why we do what we do!</h2>
     <p>
       Duis rutrum dictum libero quis rutrum. Etiam sed neque aliquam, sollicitudin ante a, gravida
@@ -9,7 +9,6 @@
       ac sodales lectus placerat quis.
     </p>
     <h2>Top trends</h2>
-    @/components/HomeAdbout.vue:8
     <img src="/images/pearls-hands.jpg" alt="Жемчужное ожерелье" class="about-image" />
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam placerat, augue a volutpat
@@ -20,20 +19,62 @@
 </template>
 
 <style scoped lang="scss">
+  @use '@/assets/scss/variables' as *;
+
   .about-container {
     display: flex;
     flex-direction: column;
-    gap: 20px;
+    gap: 0;
     align-items: center;
-    max-width: 1200px;
+    max-width: 100%;
     padding: 20px;
     margin: 0 auto;
+
+    @media (min-width: $breakpoints-l) {
+      max-width: 672px;
+    }
   }
 
   .about-image {
     width: 100%;
-    max-width: 600px;
     height: auto;
+    margin-bottom: 48px;
     border-radius: 8px;
+  }
+
+  h1 {
+    margin-bottom: 24px;
+    font-family: $font-family-primary;
+    font-size: 33px;
+    font-weight: $font-weight-medium;
+    line-height: 43px;
+  }
+
+  h2:first-of-type {
+    font-family: $font-family-primary;
+    font-size: 20px;
+    font-weight: $font-weight-regular;
+    line-height: 26px;
+    text-align: center;
+  }
+
+  p {
+    font-family: $font-family-primary;
+    font-size: 16px;
+    font-weight: $font-weight-regular;
+    line-height: 27px;
+  }
+
+  p:first-of-type {
+    margin-bottom: 46px;
+  }
+
+  h2:last-of-type {
+    align-self: flex-start;
+    margin-bottom: 23px;
+    font-family: $font-family-primary;
+    font-size: 26px;
+    font-weight: $font-weight-regular;
+    line-height: 35px;
   }
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -4,9 +4,22 @@
     <section>
       <HomeSlider />
     </section>
-    <section>
+    <section class="about-section">
       <HomeAdbout />
     </section>
     <slot />
   </main>
 </template>
+
+<style scoped lang="scss">
+  @use '@/assets/scss/variables' as *;
+
+  .about-section {
+    margin-top: 40px;
+
+    @media (min-width: $breakpoints-xl) {
+      margin-top: 90px;
+      margin-bottom: 48px;
+    }
+  }
+</style>
