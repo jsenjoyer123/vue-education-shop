68bf9443e2335b7ed41b6bdf2a459eb922cd07a3
name <user@example.com>
2026-04-30 20:03:01 +0300

    добавил базовый html и пропсы в productCard

--- components/UI/ProductCard.vue
+++ components/UI/ProductCard.vue
@@ -1,4 +1,18 @@
-<teamplate>
-    <svg>
-    <img class="product-img"/>
-</teamplate>
+<script setup lang="ts">
+  defineProps<{
+    productName: string
+    poroductImage: string
+    productStatus: string
+    productPrice: number
+  }>()
+</script>
+
+<template>
+  <div class="image-wrapper">
+    <svg class="product-status"></svg>
+    <img class="product-img" />
+    <button>ADD TO CART</button>
+  </div>
+  <h2>{{ productName }}</h2>
+  <p>{{ productPrice }}</p>
+</template>
