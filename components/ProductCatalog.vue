cfc4376aed7dfc89431d8f2883fbf4a70aa75ec4
name <user@example.com>
2026-05-01 11:11:29 +0300

    feat: добавил базовую разметку product-catalog

--- components/ProductCatalog.vue
+++ components/ProductCatalog.vue
@@ -1,5 +1,26 @@
 <template>
-  <h1>Catalog</h1>
-  <ProductFilters />
-  <ProductList />
+  <div class="catalog-container">
+    <ProductFilters class="product-filters" />
+    <ProductList class="product-list" />
+  </div>
 </template>
+
+<style scoped lang="scss">
+  .catalog-container {
+    display: flex;
+    gap: 35px;
+  }
+
+  .product-filters {
+    flex-shrink: 0;
+    width: 261px;
+    background-color: gray;
+  }
+
+  .product-list {
+    display: grid;
+    grid-template-rows: repeat(3, 300px);
+    grid-template-columns: repeat(3, 300px);
+    gap: 20px;
+  }
+</style>
